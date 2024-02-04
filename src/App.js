14894ee88378b7a35fe1784f20f65a44c709836e
sandece10 <anguesandrine38@gmail.com>
2024-02-04 19:23:27 +0100

    code source

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import audiovisuel2JPG from "./audiovisuel2.jpg"
+
+function App() {
+  return (
+    <div className="App">
+         <img src={audiovisuel2JPG} alt="audiovisuel2 jpg" />
+         <h1>loremhhbhbhbjkbjkbjkbjbbugjhbhjbhjbhbhb
+          jbhjbjkhnbjknjknkjnkjnkjnkjnkjlnklnklnklknl
+          kjbnjknbjknkjnkjnkjnkjnkjnknknjnjknjknkjnkjn
+          vgvghjvbjbhjbhjbjkbjknkjnkjnjknjknnjknjjbjhb
+         </h1>
+          
+    </div>
+  );
+}
+
+export default App;
